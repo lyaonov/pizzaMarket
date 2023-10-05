a1f49e681924e16dbdb2f4f1671a964db79c4c09
lyaonov <nalleonov@sberbank.ru>
2023-10-05 14:09:38 +0300

    first commit

---
+++ src/redux/pizza/asyncActions.ts
@@ -0,0 +1,26 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { Pizza, SearchPizzaParams } from './types';
+import pickBy from 'lodash/pickBy';
+import identity from 'lodash/identity';
+
+export const fetchPizzas = createAsyncThunk<Pizza[], SearchPizzaParams>(
+  'pizza/fetchPizzasStatus',
+  async (params) => {
+    const { sortBy, order, category, search, currentPage } = params;
+    const { data } = await axios.get<Pizza[]>(`https://pizza.nikitalieonov.repl.co/pizzas`, {
+      params: pickBy(
+        {
+          page: currentPage,
+          limit: 4,
+          category,
+          sortBy,
+          order,
+          search,
+        },
+        identity,
+      ),
+    });
+    return data;
+  },
+);
