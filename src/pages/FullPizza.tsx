11fe962bf66dae99cfbf0eee0360924c416a5278
Rofleksey <evilmorty@yandex.ru>
2023-11-15 21:52:51 +0300

    Add version 4 goals

--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -42,6 +42,12 @@ const FullPizza: React.FC = () => {
     };
 
     dispatch(addItem(item));
+
+    // @ts-ignore
+    window.ym(95160416, 'reachGoal', 'addPizza', {version: appVersion})
+
+    // @ts-ignore
+    window.ym(95160416, 'reachGoal', 'addPizzaFull', {version: appVersion})
   };
 
   React.useEffect(() => {
